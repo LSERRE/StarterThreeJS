aee2d0549ccf7c9ae143853067daf0d8f65a35d2
Louis Serre <louis.serre@gmail.com>
2015-04-29 15:57:34 +0200

    Step 1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-var params = new function() {
-}
-
 $(function () {
 	var windowWidth = window.innerWidth;
 	var windowHeight = window.innerHeight;
+
+	var params = {
+		timeSpeed: 1
+	}
 
 	/* Init ThreeJS perfomance stats */
 	var stats = initStats();
@@ -11,39 +12,101 @@ $(function () {
 	/* Init GUI */
 	var gui = new dat.GUI();
 
+	gui.add(params, 'timeSpeed', 1, 100).onChange(function(){
+        generateAnimations();
+    });
+
 	var scene, camera, renderer;
 
+	var planeteGroup = new THREE.Object3D();
+
+	var planetes = [];
+
+	var yAxis = new THREE.Vector3(0,1,0);
+
 	init();
-	animate();
+	render();
 
 	function init() {
-		/* Init Scene */
-	    scene = new THREE.Scene();
+		scene = new THREE.Scene();
 
-	    /* Init Camera */
-	    camera = new THREE.PerspectiveCamera( 75, windowWidth / windowHeight, 1, 10000 );
-	    camera.position.z =  1000;
+		camera = new THREE.PerspectiveCamera( 75, windowWidth / windowHeight, 1, 1000 );
 
-	    /* Init renderer as WebGlRenderer */
-	    renderer = new THREE.WebGLRenderer();
+		camera.position.set(0, 800, 0);
 
-	    /* Set renderer size */
-	    renderer.setSize( windowWidth, windowHeight );
+		camera.lookAt(new THREE.Vector3( 0, 0, 0 ));
 
-	    /* Append canvas with goods sizes in body  */
-	    document.body.appendChild( renderer.domElement );
+		controls = new THREE.OrbitControls( camera );
+		controls.damping = 0.2;
+		controls.addEventListener( 'change', render );
+
+		renderer = new THREE.WebGLRenderer();
+
+		renderer.setSize( windowWidth, windowHeight);
+
+		document.body.appendChild( renderer.domElement );
+
+		generatePlanetes();
+
+		generateAnimations();
+
+		scene.add(planeteGroup);
+
+		animate();
+	}
+
+	function generatePlanetes() {
+		for( var i = 0; i < sources.planetes.length; i++ ) {
+			console.log(sources.planetes[i].size);
+			var planeteGeometry = new THREE.SphereGeometry( sources.planetes[i].size / 800, 64, 64 );
+			var planeteTexture = THREE.ImageUtils.loadTexture('assets/images/' + sources.planetes[i].texture );
+			var planeteMaterial = new THREE.MeshBasicMaterial({ map: planeteTexture, side:THREE.DoubleSide  });
+			var planeteMesh = new THREE.Mesh( planeteGeometry, planeteMaterial );
+			planetes.push(planeteMesh);
+			planetes[i]["solarAngle"] = {value : 0};
+			planetes[i]["selfAngle"] = {value : 2 * Math.PI};
+			planeteGroup.add( planeteMesh );
+		}
+	}
+
+	function generateAnimations() {
+		for( var i = 0; i < sources.planetes.length; i++ ) {
+			TweenMax.to(planetes[i]["solarAngle"], sources.planetes[i].revolution / 20 / params.timeSpeed, {value: 2 * Math.PI, ease: Linear.easeNone }).repeat(-1);
+			TweenMax.to(planetes[i]["selfAngle"], sources.planetes[i].rotation / params.timeSpeed, {value: 4 * Math.PI,  onUpdate: function(){console.log(sources.planetes[2].name + "selfAngle: " + planetes[2]["selfAngle"].value )}, ease: Linear.easeNone }).repeat(-1);
+		}
 	}
 
 	function animate() {
-		/* Updates stats */
+
+		rotatePlanetes();
+		requestAnimationFrame(animate);
+		controls.update();
+		renderer.render( scene, camera );
+
+	}
+
+	function render() {
+
 		stats.update();
 
-		/* Loop on animate function when browser has an avaible frame */
-	    requestAnimationFrame( animate );
-
-	    /* Render and update canvas with scene and camera */
-	    renderer.render( scene, camera );
 	}
+
+	function rotatePlanetes() {
+		for( var i = 0; i < planeteGroup.children.length; i++ )  {
+			rotateAroundObjectAxis(planeteGroup.children[i], yAxis, planetes[i]["selfAngle"].value);
+			planeteGroup.children[i].position.x = Math.sin(planetes[i]["solarAngle"].value) * sources.planetes[i].distance / 2;
+        	planeteGroup.children[i].position.z = Math.cos(planetes[i]["solarAngle"].value) * sources.planetes[i].distance / 2;
+		}
+	}
+
+	// This function rotate an element around it self axis
+	function rotateAroundObjectAxis( object, axis, radians ) {
+	    rotObjectMatrix = new THREE.Matrix4();
+	    rotObjectMatrix.makeRotationAxis(axis.normalize(), radians);
+	    object.matrix.multiply(rotObjectMatrix);
+	    object.rotation.setFromRotationMatrix(object.matrix);
+	}
+
 
 	function initStats () {
 		var stats = new Stats();
---
+++ js/sources.js
@@ -0,0 +1,76 @@
+var sources = {
+    planetes : [
+        {
+            "name"              : "Mercure",
+            "distance"          : "58",
+            "size"              : "4900",
+            "revolution"        : "88",
+            "texture"           : "mercuremap.jpg",
+            "rotation"          : "59",
+        },
+        {
+            "name"              : "Venus",
+            "distance"          : "108",
+            "size"              : "12104",
+            "revolution"        : "224",
+            "texture"           : "venusmap.jpg",
+            "rotation"          : "243",
+        },
+        {
+            "name"              : "Terre",
+            "distance"          : "149",
+            "size"              : "12756",
+            "revolution"        : "365",
+            "texture"           : "earthmap.jpg",
+            "rotation"          : "1",
+        },
+        {
+            "name"              : "Mars",
+            "distance"          : "227",
+            "size"              : "6788",
+            "revolution"        : "687",
+            "texture"           : "marsmap.jpg",
+            "rotation"          : "1",
+        },
+        {
+            "name"              : "Jupiter",
+            "distance"          : "778",
+            "size"              : "8000",
+            "revolution"        : "4340",
+            "texture"           : "jupitermap.jpg",
+            "rotation"          : "0.41", 
+        },
+        {
+            "name"              : "Saturne",
+            "distance"          : "1426",
+            "size"              : "4900",
+            "revolution"        : "10585",
+            "texture"           : "saturnemap.jpg",
+            'rotation'          : "0.43",
+        },
+        {
+            "name"              : "Uranus",
+            "distance"          : "2871",
+            "size"              : "51152",
+            "revolution"        : "29736",
+            "texture"           : "uranusmap.jpg",
+            "roation"           : "0.6",
+        },
+        {
+            "name"              : "Neptune",
+            "distance"          : "4498",
+            "size"              : "49620",
+            "revolution"        : "30660",
+            "texture"           : "neptunemap.jpg",
+            "roation"           : "0,75",
+        },
+        {
+            "name"              : "Pluton",
+            "distance"          : "5960",
+            "size"              : "2296",
+            "revolution"        : "90155",
+            "texture"           : "plutonmap.jpg",
+            "rotation"          : "6.39",
+        }
+    ]
+}
